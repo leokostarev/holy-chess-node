0457271b5420207aadc845f53b4a8ef4f16162e1
BadCatSet <leokostarev@mail.ru>
2023-11-11 21:06:27 +0300

    pub nub test

--- server.js
+++ server.js
@@ -1,50 +1,68 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const path = require("path");
-const Pusher = require("pusher");
-const http = require("http");
+//========================================================================
+//                                 IMPORTS
+//========================================================================
+const Favicon = require("serve-favicon");
+const Cors = require("cors");
+const Express = require("express");
+const BodyParser = require("body-parser");
+// const Pusher = require("pusher-js");
+const Path = require("path");
+const Http = require("http");
+const FileLog = require("log-to-file");
 
-const app = express();
-const server = http.createServer(app);
-
+//========================================================================
+//                                 CONFIG
+//========================================================================
+const app = Express();
+const server = Http.createServer(app);
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "templates"));
+app.set("views", Path.join(__dirname, "templates"));
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname, "/static")));
+app.use(Cors());
+app.use(Express.json());
+app.use(BodyParser.urlencoded({extended: false}));
+app.use(Express.static(Path.join(__dirname, "/static")));
+app.use(Favicon(Path.join(__dirname, "/static/favicon.ico")));
 
 
-const pusher = new Pusher({
-    appId:   "1702906",
-    key:     "96a6c5bc9ce712f90ddb",
-    secret:  "e8bbb2802116a4bba605",
-    cluster: "eu",
-    useTLS:  true,
-});
-
+//========================================================================
+//                                 ROUTES
+//========================================================================
 app.get("/", (req, res) => {
     res.render("index");
 });
 
+
+app.get("/chat", (req, res) => {
+    res.render("chat");
+});
+
+
 app.get("/menu", (req, res) => {
     res.render("menu");
 });
+
 
 app.post("/menu", (req, res) => {
     console.log(req.body);
     res.redirect("/game");
 });
 
+
 app.get("/game", (req, res) => {
     res.render("game");
 });
 
+
+//========================================================================
+//                                 SETUP
+//========================================================================
+
 setInterval(() => {
-    pusher.trigger("index-ch", "message", {
-        message: "yo mama",
-    });
-}, 5000);
+    },
+    3000);
+
 
 if (process.env.LOCAL) {
     console.log("LOCAL!", process.env.LOCAL);
