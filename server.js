ef7f107d810036ffcbb81ef7e9e29b067fefdea8
BadCatSet <leokostarev@mail.ru>
2023-11-11 23:59:29 +0300

    pub nub test2

--- server.js
+++ server.js
@@ -5,10 +5,9 @@ const Favicon = require("serve-favicon");
 const Cors = require("cors");
 const Express = require("express");
 const BodyParser = require("body-parser");
-// const Pusher = require("pusher-js");
 const Path = require("path");
+const PubNub = require("pubnub");
 const Http = require("http");
-const FileLog = require("log-to-file");
 
 //========================================================================
 //                                 CONFIG
@@ -24,6 +23,52 @@ app.use(Express.json());
 app.use(BodyParser.urlencoded({extended: false}));
 app.use(Express.static(Path.join(__dirname, "/static")));
 app.use(Favicon(Path.join(__dirname, "/static/favicon.ico")));
+
+
+const pubnub = new PubNub({
+    publishKey:   "pub-c-78f8aeaf-c800-4522-a006-df4b63f56915",
+    subscribeKey: "sub-c-fcf7a377-4aed-450e-a840-5a9aae9969d8",
+    userId:       "server",
+});
+
+pubnub.subscribe({channels: ["to_server"]});
+
+// add listener
+pubnub.addListener({
+    status:  (statusEvent) => {
+        if (statusEvent.category === "PNConnectedCategory") {
+            console.log("Connected");
+        }
+    },
+    message: (message) => {
+        publishMessage("Hi!:" + message.message);
+    },
+});
+
+function publishMessage(message) {
+    pubnub.publish({
+        channel: "from_server",
+        message: message,
+    });
+}
+
+setInterval(() => {
+    publishMessage("Hi mom!");
+}, 3000);
+
+//
+// const readline = require("readline").createInterface({
+//     input:  process.stdin,
+//     output: process.stdout,
+// });
+//
+// readline.setPrompt("");
+// readline.prompt();
+// // publish after hitting return
+// readline.on("line", (message) => {
+//     publishMessage(message).then(r => {
+//     });
+// });
 
 
 //========================================================================
@@ -58,10 +103,6 @@ app.get("/game", (req, res) => {
 //========================================================================
 //                                 SETUP
 //========================================================================
-
-setInterval(() => {
-    },
-    3000);
 
 
 if (process.env.LOCAL) {
